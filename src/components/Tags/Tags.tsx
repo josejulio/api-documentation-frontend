84a6eecf952b694cf09e722576827194e7e04435
Josejulio Martínez <josejulio@users.noreply.github.com>
2023-03-14 12:14:51 -0600

    Remove console.log

--- src/components/Tags/Tags.tsx
+++ src/components/Tags/Tags.tsx
@@ -2,10 +2,7 @@ import {FunctionComponent, PropsWithChildren} from "react";
 import {LabelGroup} from "@patternfly/react-core";
 
 export const Tags: FunctionComponent<PropsWithChildren> = ({children}) => {
-    return <LabelGroup onClick={() => {
-        console.log('label group click');
-    }
-    }>
+    return <LabelGroup>
         {children}
     </LabelGroup>;
 };
